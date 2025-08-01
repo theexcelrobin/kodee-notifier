94a26ac61ec8d6e91a002526ffaaf6301ce40127
theexcelrobin <excellentrobinson1@gmail.com>
2025-08-01 23:23:03 +0100

    initial commits

--- internal/email/main.go
+++ internal/email/main.go
@@ -1,5 +1,11 @@
 package email
 
+import (
+	"fmt"
+
+	"github.com/joho/godotenv"
+)
+
 type Email struct {
 	Address  string
 	Password string
@@ -8,5 +14,9 @@ type Email struct {
 }
 
 func NewEmail() (*Email, error) {
+	if err := godotenv.Load(); err != nil {
+		return nil, fmt.Errorf("error loading .env file: %s", err.Error())
+	}
+
 	return &Email{}, nil
 }
