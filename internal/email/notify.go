00c89656bd066cb1ecbc729dffd33581ec63b606
theexcelrobin <excellentrobinson1@gmail.com>
2025-08-02 16:52:50 +0100

    initial commits

--- internal/email/notify.go
+++ internal/email/notify.go
@@ -6,7 +6,7 @@ import (
 )
 
 func (e *Email) Notify(email, subject, content string) error {
-	msg := []byte(fmt.Sprintf("Subject: %s\r\n\r\n%s", subject, content))
+	msg := fmt.Appendf(nil, "Subject: %s\r\n\r\n%s", subject, content)
 
 	if err := smtp.SendMail(e.Host+":"+e.Port, e.Auth, e.Address, []string{email}, msg); err != nil {
 		return err
